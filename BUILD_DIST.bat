09d8a6649353f397981ad768f22a9653d0bb9069
JAMAGAX <jamagax@geometrica.xyz>
2026-03-02 16:18:35 -0500

    Update BUILD_DIST to include js and clients

--- BUILD_DIST.bat
+++ BUILD_DIST.bat
@@ -9,7 +9,9 @@ copy /Y index.html dist\
 copy /Y style.css dist\
 copy /Y script.js dist\
 
-echo [BUILD] Copying assets...
+echo [BUILD] Copying assets and nested folders...
 xcopy /E /I /Y assets dist\assets
+xcopy /E /I /Y js dist\js
+xcopy /E /I /Y clients dist\clients
 
 echo [BUILD] Success! dist/ folder is ready for FTP upload.
